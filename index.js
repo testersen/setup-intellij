1be6ae8b350cf5cfd558a9433d47e6aa485efd4b
Jameson <james.bradlee@telenor.no>
2023-07-08 15:14:06 +0200

    test

--- index.js
+++ index.js
@@ -2,24 +2,18 @@ const core = require("@actions/core");
 const tc = require("@actions/tool-cache");
 
 async function setup() {
-	core.startGroup("installing");
 	const version = core.getInput("version");
 	core.info(`${version}`);
 	const tarball = await tc.downloadTool(
 		`https://download-cdn.jetbrains.com/idea/ideaIC-${version}.tar.gz`,
 	);
 	core.info(`${tarball}`);
-	const pathToIdea = await tc.extractTar(tarball);
+	const pathToIdea =
+		(await tc.extractTar(tarball)) +
+		"/" +
+		require("fs").readdirSync(pathToIdea)[0];
 	core.addPath(`${pathToIdea}/bin`);
 	core.info(`${pathToIdea}/bin`);
-	core.info(
-		JSON.stringify(
-			require("fs").readdirSync(pathToIdea, { recursive: true }),
-			null,
-			2,
-		),
-	);
-	core.endGroup();
 }
 
 module.exports = setup;
