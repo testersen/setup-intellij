2645576ee0bc4dcb96b39856e7e11a299a263777
Jameson <james.bradlee@telenor.no>
2023-07-08 15:01:41 +0200

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require("@actions/core");
 const tc = require("@actions/tool-cache");
 
 async function setup() {
+	core.startGroup("installing");
 	const version = core.getInput("version");
 	core.info(`${version}`);
 	const tarball = await tc.downloadTool(
@@ -11,6 +12,9 @@ async function setup() {
 	const pathToIdea = await tc.extractTar(tarball);
 	core.addPath(`${pathToIdea}/bin`);
 	core.info(`${pathToIdea}/bin`);
+	core.endGroup();
 }
 
 module.exports = setup;
+
+setup().catch(core.setFailed);
